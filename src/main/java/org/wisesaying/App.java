db1df77ecb8e1b64298e22b775d83731161b902d
4est.lil.dev <carneo.dev@gmail.com>
2024-11-20 10:07:15 +0900

    1 Step: 종료 기능 추가

--- src/main/java/org/wisesaying/App.java
+++ src/main/java/org/wisesaying/App.java
@@ -1,8 +1,26 @@
 package org.wisesaying;
 
-public class App {
-    public void run() {
-        System.out.print("== 명언 ==\n명령 : ");
+import java.util.Scanner;
 
+public class App {
+    public static void printMenu() {
+        System.out.println("====== 명언 앱 ======");
+        System.out.println(">> 1. 종료");
+        System.out.print  ("입력 : ");
+    }
+
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            printMenu();
+            String choiceMenu = sc.nextLine();
+
+            // 종료 메뉴
+            if(choiceMenu.equals("종료") || choiceMenu.equals("1")) {
+                System.out.println("앱이 종료되었습니다.\n");
+            }
+
+        }
     }
 }
